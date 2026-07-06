0c866f86d59ddf609f73fb47d3c839a75b3b110e
Cue-designs <cajemma122@gmail.com>
2026-07-06 15:31:29 +0100

    change ray color

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
         <div style={{ zIndex: -1, position: "absolute", inset: 0, top: 0 }}>
           <LightRays
             raysOrigin="top-center"
-            raysColor="#ffffff"
+            raysColor="#01e645"
             raysSpeed={1}
             lightSpread={2}
             rayLength={3}
