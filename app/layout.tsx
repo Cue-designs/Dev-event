beedb6bad973be3e194b32608cc0b0947e16d05a
Cue-designs <cajemma122@gmail.com>
2026-07-06 13:32:05 +0100

    Rays: made the light rays working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
+import LightRays from "@/components/LightRays";
 import Navbar from "@/components/Navbar";
 
 const schibstedGrotesk = Schibsted_Grotesk({
@@ -29,9 +30,25 @@ export default function RootLayout({
       className={`${schibstedGrotesk.variable} ${martianMono.variable} h-full antialiased`}
     >
       <body className="min-h-screen ">
+        <div style={{ width: "100%", height: "600px", position: "relative" }}>
+          <LightRays
+            raysOrigin="top-center"
+            raysColor="#ffffff"
+            raysSpeed={1}
+            lightSpread={0.5}
+            rayLength={3}
+            followMouse={true}
+            mouseInfluence={0.1}
+            noiseAmount={0}
+            distortion={0}
+            className="custom-rays"
+            pulsating={false}
+            fadeDistance={1}
+            saturation={1}
+          />
+        </div>
         <Navbar />
         <div className="absolute inset-0 top-0 z-[-1] min-h-screen"></div>
-
         <main>{children}</main>
       </body>
     </html>
